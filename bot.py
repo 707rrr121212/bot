cc1fab6d271d0cca29b1a0e0e9bb9c5862ea7e40
707rrr121212 <707rrr12@gmail.com>
2025-08-22 11:01:45 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = os.getenv("BOT_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я твой юридический помощник. Задай вопрос!")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer("Ваш запрос принят, скоро будет обработан!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
